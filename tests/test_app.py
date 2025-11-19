86526efae7260149ce82cfb535eb5cf7eb8f9133
Aruniisu <maduwanthiliyanage6953@gmail.com>
2025-11-19 16:44:03 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,14 @@
-from app.app import app
+import sys
+import os
+
+# Add project root to PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app.app import app  # now works
 
 def test_home():
     client = app.test_client()
     response = client.get("/")
+    
     assert response.status_code == 200
-    assert b"Hello, DevOps!" in response.data
+    assert b"Hello" in response.data
